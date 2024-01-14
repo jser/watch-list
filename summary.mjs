dd966b460b714399e452d5eb34fbfd6e8a6b18f7
azu <azuciao@gmail.com>
2024-01-14 15:01:22 +0900

    docs: add no rss list

--- summary.mjs
+++ summary.mjs
@@ -13,9 +13,18 @@ const title = options.values.title ?? "Summary";
 const readJSON = async (path) => {
     return JSON.parse(fs.readFileSync(path, "utf8"));
 };
+/** @type import("./src/watch-list.ts").WatchItem[] **/
 const watchList = await readJSON("data/watch-list.json");
+/** @type import("./src/rss-list.ts").FeedItem[] **/
 const feedList = await readJSON("data/feed-list.json");
+/** @type import("./src/rss-list.ts").FeedItem[] **/
 const opmlList = await readJSON("data/opml-list.json");
+const noHaveFeedList = watchList.filter((site) => {
+    return !feedList.some((feed) => {
+        return feed.url === site.url;
+    });
+});
+
 const summary = {
     total: watchList.length,
     feeds: feedList.length,
@@ -25,4 +34,14 @@ console.log(`# ${title}
 - total: ${summary.total}
 - feeds: ${summary.feeds}
 - opmls: ${summary.opmls}
+
+<details>
+<summary>No RSS list: ${noHaveFeedList.length}</summary>
+
+${noHaveFeedList.map((site) => {
+    return `- <${site.url}>`;
+}).join("\n")}
+
+</details>
+
 `);
